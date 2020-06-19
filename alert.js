c88dd020eb5b39ad798ddd84fb1791016b4c8da1
Roman Romanenko <twaresare@gmail.com>
2020-06-19 15:40:55 +0300

    .

--- alert.js
+++ alert.js
@@ -1,21 +1,10 @@
 'use strict';
-
 // console.log(Number.MAX_VALUE);
 
-<<<<<<< HEAD
 function checkAge(age) {
   return (age >= 18) || confirm("Does your mother know you are here?")
 }
 
 let age;
 age = +prompt("Your age: ", "");
-
 alert(checkAge(age));
-=======
-function SayHi() {
-    alert("Hello")
-    return 0
-}
-
-alert(SayHi())
->>>>>>> 2b04cd43805c6126848fdfe455802476c97da56c
