7c02029f0afd0ec6f08027156f6d759b81726fc5
Roman Romanenko <twaresare@gmail.com>
2020-06-19 15:33:23 +0300

    Merging

--- alert.js
+++ alert.js
@@ -1,9 +1,11 @@
 'use strict';
 // console.log(Number.MAX_VALUE);
-let age = prompt("Enter your age", "15");
 
-if (age >= 14 && age <= 90) {
-  alert(`Your age is ${age}` );
-} else {
-  alert("You are too young!");
+function checkAge(age) {
+  return (age >= 18) || confirm("Does your mother know you are here?")
 }
+
+let age;
+age = +prompt("Your age: ", "");
+
+alert(checkAge(age));
