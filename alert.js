2b04cd43805c6126848fdfe455802476c97da56c
Roman Romanenko <roman.romanenko@ix.co>
2020-06-19 12:47:46 +0300

    .

--- alert.js
+++ alert.js
@@ -1,9 +1,10 @@
 'use strict';
-// console.log(Number.MAX_VALUE);
-let age = prompt("Enter your age", "15");
 
-if (age >= 14 && age <= 90) {
-  alert(`Your age is ${age}` );
-} else {
-  alert("You are too young!");
+// console.log(Number.MAX_VALUE);
+
+function SayHi() {
+    alert("Hello")
+    return 0
 }
+
+alert(SayHi())
