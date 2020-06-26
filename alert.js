4ba51007b2e0256df9507e03360848774c258728
Roman Romanenko <roman.romanenko@ix.co>
2020-06-26 15:48:04 +0300

    .

--- alert.js
+++ alert.js
@@ -1,18 +1,28 @@
 'use strict';
 
-// TODO https://javascript.info/task/maximal-subarray
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
 
-// function getMaxSubSum(arr) {
-//   while (arr[arr.length - 1] <= 0) { arr.length--; }
-//   let sum = 0;
-//   let start = -1;
-//   for (let i=0; i<arr.length;i++) {
-//     if (sum + arr[i] <= 0) {
-//       sum = 0;
-//       start = i+1;
-//     } else {
-//       sum += arr[i];
-//     };
-//   }
-//   return sum;
-// };
+console.log(groupById(users));
+
+function groupById(arr) {
+
+
+//    for (let i in arr) {
+//        newArr[arr[i].id] = arr[i]
+//    }
+    return arr.reduce( (newArr, cur, curInd) => { newArr[cur.id] = cur; return newArr }, {} );
+}
+
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
